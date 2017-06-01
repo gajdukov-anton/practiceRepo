7f91449d1c47ce1b650835c52fce9d65413f5e78
gajdukov-anton <antony12344@yandex.ru>
2017-06-01 11:45:22 +0300

    Add files via upload

---
+++ задание1/js/script.js
@@ -0,0 +1,6 @@
+function change_color() {
+    document.getElementById('text').onclick.style.color = 'green';
+};
+/*var element = document.getElementById('change');
+element.onlclick = change_color();*/
+document.addEventListener("DOMContentLoaded", "ready");
